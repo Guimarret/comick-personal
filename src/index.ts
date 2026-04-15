22430548e5dd809f9a5d8f3c4435dc002ca823b8
Guimas <guimarret@gmail.com>
2026-04-14 21:40:12 -0300

    starting the api setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
+import { login_auth, register_user } from './auth/login';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {   
+	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		switch (url.pathname) {
 			case '/':
-				return new Response('')
+				return new Response('');
+			case '/api/login': {
+				if (request.method !== 'POST') {
+					return new Response('Method Not Allowed', { status: 405 });
+				}
+				const { identifier, password } = await request.json<{ identifier: string; password: string }>();
+				const result = await login_auth(identifier, password, env.comick_personal_db);
+				if (!result.success) {
+					return Response.json({ success: false, error: result.error }, { status: 401 });
+				}
+				return Response.json({ success: true });
+			}
+			case '/api/create_user': {
+				if (request.method !== 'POST') {
+					return new Response('Method Not Allowed', { status: 405 });
+				}
+				const { email, password, username } = await request.json<{ email: string; password: string, username: string}>();
+				const result = await register_user(email, password, env.comick_personal_db, username);
+				if (!result.success) {
+					return Response.json({ success: false, error: result.error }, { status: 400 });
+				}
+				return Response.json({ success: true });
+			}
 			case '/message':
 				return new Response('Hello, World!');
 			case '/random':
